fad41db3ca925910c91dcef331cd06ccc3ef4625
Howlseye <nikolasagala@gmail.com>
2026-05-23 22:06:22 +0700

    Membuat item dapat diklik

--- composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.nikola0055.kmp.ui.screen
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +12,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -25,11 +31,15 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.nikola0055.kmp.model.Catatan
 import kmp.composeapp.generated.resources.*
+import kotlinx.coroutines.launch
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    // Pengganti Toast
+    val snackbarHostState = remember { SnackbarHostState() }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -41,16 +51,26 @@ fun MainScreen() {
                     titleContentColor = MaterialTheme.colorScheme.primary,
                 )
             )
+        },
+        // Pengganti Toast
+        snackbarHost = {
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.padding(bottom = 16.dp)
+            )
         }
     ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
+        ScreenContent(Modifier.padding(innerPadding), snackbarHostState)
     }
 }
 
 @Composable
-fun ScreenContent(modifier: Modifier = Modifier) {
+fun ScreenContent(modifier: Modifier = Modifier, snackbarHostState: SnackbarHostState) {
     val viewModel: MainViewModel = viewModel{ MainViewModel() }
     val data = viewModel.data
+
+    // Pengganti Toast
+    val scope = rememberCoroutineScope()
 
     if (data.isEmpty()) {
         Column(
@@ -66,7 +86,16 @@ fun ScreenContent(modifier: Modifier = Modifier) {
             modifier = modifier.fillMaxSize()
         ) {
             items(data) {
-                ListItem(catatan = it)
+                val pesan = stringResource(Res.string.x_diklik, it.judul)
+                ListItem(catatan = it) {
+                    // Pengganti Toast
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = pesan,
+                            duration = SnackbarDuration.Short
+                        )
+                    }
+                }
                 HorizontalDivider()
             }
         }
@@ -74,9 +103,11 @@ fun ScreenContent(modifier: Modifier = Modifier) {
 }
 
 @Composable
-fun ListItem(catatan: Catatan) {
+fun ListItem(catatan: Catatan, onClick: () -> Unit) {
     Column(
-        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        modifier = Modifier.fillMaxWidth()
+            .clickable { onClick() }
+            .padding(16.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         Text(
