5b0e920f7adca65502f49b22da1653d3230adc37
Howlseye <nikolasagala@gmail.com>
2026-05-25 14:18:52 +0700

    Reset

--- composeApp/src/commonMain/kotlin/com/nikola0055/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/App.kt
@@ -1,79 +1,21 @@
 package com.nikola0055.kmp
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import kmp.composeapp.generated.resources.*
-import org.jetbrains.compose.resources.stringResource
+import com.nikola0055.kmp.ui.screen.MainScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
 fun App() {
     val darkTheme = isSystemInDarkTheme()
 
     MaterialTheme(
         colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme()
     ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(stringResource(Res.string.app_name))
-                    },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary
-                    )
-                )
-            }
-        ) { innerPadding ->
-            ScreenContent(Modifier.padding(innerPadding))
-        }
-    }
-}
-
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
-@Composable
-fun ScreenContent(
-    modifier: Modifier = Modifier
-) {
-    var number by remember { mutableIntStateOf(0) }
-
-    Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = number.toString(),
-            style = MaterialTheme.typography.displayLarge,
-        )
-        Button(
-            onClick = { number++ },
-            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            Text(stringResource(Res.string.count))
-        }
+        MainScreen()
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/MainScreen.kt
@@ -0,0 +1,259 @@
+package com.nikola0055.kmp.ui.screen
+
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kmp.composeapp.generated.resources.*
+import kmp.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
+import kotlin.math.pow
+import kotlin.math.roundToInt
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(Res.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(modifier: Modifier = Modifier) {
+    var berat by remember { mutableStateOf("") }
+    var beratError by remember { mutableStateOf(false) }
+
+    var tinggi by remember { mutableStateOf("") }
+    var tinggiError by remember { mutableStateOf(false) }
+
+    val radioOptions = listOf(
+        stringResource(Res.string.pria),
+        stringResource(Res.string.wanita)
+    )
+    var gender by remember { mutableStateOf(radioOptions[0]) }
+
+    var bmi by remember { mutableFloatStateOf(0f) }
+    val kategoriList = listOf(
+        Res.string.kurus,
+        Res.string.ideal,
+        Res.string.gemuk
+    )
+    var kategori by remember { mutableIntStateOf(0) }
+
+    Column(
+        modifier = modifier.fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = stringResource(Res.string.bmi_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = berat,
+            onValueChange = { beratValue ->
+                //Memastikan input berupa angka di desktop
+                if (beratValue.all { it.isDigit() || it == '.' } && beratValue != ".") {
+                    berat = beratValue
+                }
+            },
+            label = { Text(text = stringResource(Res.string.berat_badan)) },
+            trailingIcon = { IconPicker(beratError, "kg") },
+            supportingText = { ErrorHint(beratError) },
+            isError = beratError,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = tinggi,
+            onValueChange = { tinggiValue ->
+                if (tinggiValue.all { it.isDigit() || it == '.' } && tinggiValue != ".") {
+                    tinggi = tinggiValue
+                }
+            },
+            label = { Text(text = stringResource(Res.string.tinggi_badan)) },
+            trailingIcon = { IconPicker(tinggiError, "cm") },
+            supportingText = { ErrorHint(tinggiError) },
+            isError = tinggiError,
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Row(
+            modifier = Modifier
+                .padding(top = 6.dp)
+                .border(1.dp, Color.Gray, RoundedCornerShape(4.dp))
+        ) {
+            radioOptions.forEach { text ->
+                GenderOption(
+                    label = text,
+                    isSelected = gender == text,
+                    modifier = Modifier
+                        .selectable(
+                            selected = gender == text,
+                            onClick = { gender = text },
+                            role = Role.RadioButton
+                        )
+                        .weight(1f)
+                        .padding(16.dp)
+                )
+            }
+        }
+        Button(
+            onClick = {
+                beratError = (berat == "" || berat == "0")
+                tinggiError = (tinggi == "" || tinggi == "0")
+                if (beratError || tinggiError) return@Button
+
+                bmi = hitungBmi(berat.toFloat(), tinggi.toFloat())
+                kategori = getKategori(bmi, gender == radioOptions[0])
+            },
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal=32.dp, vertical=16.dp)
+        ) {
+            Text(text = stringResource(Res.string.hitung))
+        }
+
+        if (bmi != 0f) {
+            HorizontalDivider(
+                modifier = Modifier.padding(vertical = 8.dp),
+                thickness = 1.dp
+            )
+            Text(
+                text = stringResource(Res.string.bmi_x, ((bmi * 100).roundToInt() / 100f).toString()),
+                style = MaterialTheme.typography.titleLarge
+            )
+            Text(
+                text = stringResource(kategoriList[kategori]).uppercase(),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+    }
+}
+
+@Composable
+fun GenderOption(label: String, isSelected: Boolean, modifier: Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        RadioButton(selected = isSelected, onClick = null)
+        Text(
+            text = label,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 8.dp)
+        )
+    }
+}
+
+@Composable
+fun IconPicker(isError: Boolean, unit: String) {
+    if (isError) {
+        Icon(imageVector = Icons.Filled.Warning, contentDescription = null)
+    } else {
+        Text(text = unit)
+    }
+}
+
+@Composable
+fun ErrorHint(isError: Boolean) {
+    if (isError) {
+        Text(text = stringResource(Res.string.input_invalid))
+    }
+}
+
+private fun hitungBmi(berat: Float, tinggi: Float): Float {
+    return berat / (tinggi / 100).pow(2)
+}
+
+private fun getKategori(bmi: Float, isMale: Boolean): Int {
+    return if (isMale) {
+        when {
+            bmi < 20.5 -> 0
+            bmi >= 27.0 -> 2
+            else -> 1
+        }
+    } else {
+        when {
+            bmi < 18.5 -> 0
+            bmi >= 25.0 -> 2
+            else -> 1
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreviewLight() {
+    MaterialTheme {
+        MainScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreviewDark() {
+    MaterialTheme(colorScheme = darkColorScheme()) {
+        MainScreen()
+    }
+}
