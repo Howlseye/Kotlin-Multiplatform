fbc71503848789965c72eea8ca5bee65ada4d7f6
Howlseye <nikolasagala@gmail.com>
2026-05-25 12:35:34 +0700

    Memisahkan Composable dari Activity

--- composeApp/src/commonMain/kotlin/com/nikola0055/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/App.kt
@@ -1,20 +1,12 @@
 package com.nikola0055.kmp
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import kmp.composeapp.generated.resources.*
-import org.jetbrains.compose.resources.stringResource
+import com.nikola0055.kmp.ui.screen.MainScreen
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -24,52 +16,6 @@ fun App() {
     MaterialTheme(
         colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme()
     ) {
-        MainScreen()
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun MainScreen() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = stringResource(Res.string.app_name))
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary,
-                )
-            )
-        }
-    ) { innerPadding ->
-        ScreenContent(Modifier.padding(innerPadding))
-    }
-}
-
-@Composable
-fun ScreenContent(
-    modifier: Modifier
-) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainScreenPreviewLight() {
-    MaterialTheme {
-        MainScreen()
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MainScreenPreviewDark() {
-    MaterialTheme(colorScheme = darkColorScheme()) {
         MainScreen()
     }
 }
---
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/MainScreen.kt
@@ -0,0 +1,62 @@
+package com.nikola0055.kmp.ui.screen
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kmp.composeapp.generated.resources.Res
+import kmp.composeapp.generated.resources.app_name
+import org.jetbrains.compose.resources.stringResource
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(Res.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun ScreenContent(
+    modifier: Modifier
+) {
+    Text(
+        text = "Hello Android!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreviewLight() {
+    MaterialTheme {
+        MainScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreviewDark() {
+    MaterialTheme(colorScheme = darkColorScheme()) {
+        MainScreen()
+    }
+}
