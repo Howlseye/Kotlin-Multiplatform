71cdfb1ac74fba603ab0af7db40a22f1b236182b
Howlseye <nikolasagala@gmail.com>
2026-05-25 12:51:53 +0700

    Meminta input data dari pengguna

--- composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/MainScreen.kt
@@ -1,17 +1,30 @@
 package com.nikola0055.kmp.ui.screen
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
-import kmp.composeapp.generated.resources.Res
+import androidx.compose.ui.unit.dp
+import kmp.composeapp.generated.resources.*
 import kmp.composeapp.generated.resources.app_name
 import org.jetbrains.compose.resources.stringResource
 
@@ -36,13 +49,53 @@ fun MainScreen() {
 }
 
 @Composable
-fun ScreenContent(
-    modifier: Modifier
-) {
-    Text(
-        text = "Hello Android!",
-        modifier = modifier
-    )
+fun ScreenContent(modifier: Modifier = Modifier) {
+    var berat by remember { mutableStateOf("") }
+    var tinggi by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        Text(
+            text = stringResource(Res.string.bmi_intro),
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = berat,
+            onValueChange = { beratValue ->
+                //Memastikan input berupa angka di desktop
+                if (beratValue.all { it.isDigit() || it == '.' } && beratValue != ".") {
+                    berat = beratValue
+                }
+            },
+            label = { Text(text = stringResource(Res.string.berat_badan)) },
+            trailingIcon = { Text(text = "kg") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Next
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedTextField(
+            value = tinggi,
+            onValueChange = { tinggiValue ->
+                if (tinggiValue.all { it.isDigit() || it == '.' } && tinggiValue != ".") {
+                    tinggi = tinggiValue
+                }
+            },
+            label = { Text(text = stringResource(Res.string.tinggi_badan)) },
+            trailingIcon = { Text(text = "cm") },
+            singleLine = true,
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Number,
+                imeAction = ImeAction.Done
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
 }
 
 @Preview(showBackground = true)
