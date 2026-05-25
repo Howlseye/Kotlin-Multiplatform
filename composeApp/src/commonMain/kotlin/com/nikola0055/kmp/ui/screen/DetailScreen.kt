819a0d04bae77772447cdd05133682be199d46b6
Howlseye <nikolasagala@gmail.com>
2026-05-25 17:25:33 +0700

    Meng-update data di database

--- composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/DetailScreen.kt
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/ui/screen/DetailScreen.kt
@@ -101,6 +101,8 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
 
                         if (id == null) {
                             viewModel.insert(judul, catatan)
+                        } else {
+                            viewModel.update(id, judul, catatan)
                         }
                         navController.popBackStack()
                     }) {
