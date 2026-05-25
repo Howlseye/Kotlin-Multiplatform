4df0be615610d97e384a84dae15d025b0e9b35d3
Howlseye <nikolasagala@gmail.com>
2026-05-25 11:12:08 +0700

    Menambahkan Top App Bar

--- composeApp/src/commonMain/kotlin/com/nikola0055/kmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/nikola0055/kmp/App.kt
@@ -1,15 +1,8 @@
 package com.nikola0055.kmp
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,62 +11,65 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import kmp.composeapp.generated.resources.*
 import org.jetbrains.compose.resources.stringResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-@Preview
 fun App() {
     val darkTheme = isSystemInDarkTheme()
 
     MaterialTheme(
         colorScheme = if (darkTheme) darkColorScheme() else lightColorScheme()
     ) {
-        Scaffold(
-            topBar = {
-                TopAppBar(
-                    title = {
-                        Text(stringResource(Res.string.app_name))
-                    },
-                    colors = TopAppBarDefaults.topAppBarColors(
-                        containerColor = MaterialTheme.colorScheme.primaryContainer,
-                        titleContentColor = MaterialTheme.colorScheme.primary
-                    )
-                )
-            }
-        ) { innerPadding ->
-            ScreenContent(Modifier.padding(innerPadding))
-        }
+        MainScreen()
     }
 }
 
-@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = stringResource(Res.string.app_name))
+                },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                )
+            )
+        }
+    ) { innerPadding ->
+        ScreenContent(Modifier.padding(innerPadding))
+    }
+}
+
 @Composable
 fun ScreenContent(
-    modifier: Modifier = Modifier
+    modifier: Modifier
 ) {
-    var number by remember { mutableIntStateOf(0) }
+    Text(
+        text = "Hello Android!",
+        modifier = modifier
+    )
+}
 
-    Column(
-        modifier = modifier.fillMaxSize().padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = number.toString(),
-            style = MaterialTheme.typography.displayLarge,
-        )
-        Button(
-            onClick = { number++ },
-            modifier = Modifier.fillMaxWidth(0.5f).padding(top = 16.dp),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            Text(stringResource(Res.string.count))
-        }
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreviewLight() {
+    MaterialTheme {
+        MainScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MainScreenPreviewDark() {
+    MaterialTheme(colorScheme = darkColorScheme()) {
+        MainScreen()
     }
 }
